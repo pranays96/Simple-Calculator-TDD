036f68801f3f07d55d7c91563b749b548780b577
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 13:29:12 +0530

    Add another test case to handle two numbers separated by a comma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,13 @@ public class Main {
         // Testing different cases-
         int result1 = calculator.add("");           // Empty string
         int result2 = calculator.add("1");          // Single number
+        int result3 = calculator.add("2,3");        // Two numbers separated by comma
+
+
 
         // Print the results
         System.out.println("Result 1: " + result1); // Expected output: 0
         System.out.println("Result 2: " + result2); // Expected output: 1
+        System.out.println("Result 3: " + result3); // Expected output: 3
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -15,6 +15,13 @@ public class StringCalculatorTest {
         int result = calculator.add("1");
         Assertions.assertEquals(1, result);
     }
+    @Test
+    void testAddTwoNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,2");
+        Assertions.assertEquals(3, result);
+    }
+
 
 }
 

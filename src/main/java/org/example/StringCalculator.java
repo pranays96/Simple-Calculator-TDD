9b80704d61c1ed5f7134d4c8300b4b956bd7a4c3
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 13:09:21 +0530

    Update the add method in StringCalculator to handle a single number

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        } else {
+            return Integer.parseInt(numbers);
+        }
     }
 }

8d46c3b22e1b1355f4bf8a975414bc86bd8eade2
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 13:03:16 +0530

    Add a method add that takes a string argument and returns an integer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringCalculator calculator = new StringCalculator();
+
+        // Testing different cases-
+        int result1 = calculator.add("");           // Empty string
+
+        // Print the results
+        System.out.println("Result 1: " + result1); // Expected output: 0
     }
 }

df04f0519e11e34efb0e552c9fba1c91205e51b7
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 13:57:28 +0530

    Update the add method to handle custom delimiters by checking the input string

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -5,7 +5,12 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         } else {
-            String[] numberArray = numbers.split("[,\n]");
+            String delimiter = ",";
+            if (numbers.startsWith("//")) {
+                delimiter = numbers.substring(2, numbers.indexOf("\n"));
+                numbers = numbers.substring(numbers.indexOf("\n") + 1);
+            }
+            String[] numberArray = numbers.split("[" + delimiter + "\n]");
             int sum = 0;
             for (String number : numberArray) {
                 sum += Integer.parseInt(number);

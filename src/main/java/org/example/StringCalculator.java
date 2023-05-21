34bd13cd627ecaec385369ba463484fa7369ca2e
pranays96 <pranay.sahu14@gmail.com>
2023-05-21 13:35:34 +0530

    Update the add method to handle new lines by modifying the split regex

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -5,7 +5,7 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         } else {
-            String[] numberArray = numbers.split(",");
+            String[] numberArray = numbers.split("[,\n]");
             int sum = 0;
             for (String number : numberArray) {
                 sum += Integer.parseInt(number);
